5e494862f02e5d4673154843bfaa1679ce73f520
Sachin Sharma <krisa1826@gmail.com>
2021-03-23 14:46:36 +0530

    project setup

---
+++ communityForum/accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+
+
+
+
+#Dashboard Styling
+admin.site.site_header = "ATTC Community Forum"
+admin.site.site_title = "Developers Console"
+admin.site.index_title = "Community Forum Database"
