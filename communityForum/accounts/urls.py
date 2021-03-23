7a1ba166d2edf040f58e7f01945efe40abe2438c
Sachin Sharma <krisa1826@gmail.com>
2021-03-23 22:56:52 +0530

    added login

--- communityForum/accounts/urls.py
+++ communityForum/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import user_login, user_logout
 
 urlpatterns = [
-
+    path('login/', user_login, name="login"),
+    path('logout/', user_login, name="logout")
 ]
--- communityForum/accounts/views.py
+++ communityForum/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import  AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/admin', user)
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
+
+
+
+
+@login_required(login_url='/login')
+def user_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("login")
+
